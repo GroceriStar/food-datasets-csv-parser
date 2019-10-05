6b70dada25027a705fac5b8b8c4795add95e63ed
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-10-06 00:38:24 +0300

    testing getHeader

--- debug/parseFoodComposition.js
+++ debug/parseFoodComposition.js
@@ -1,3 +1,6 @@
-// const { parseFoodComposition } = require('../dist/index.cjs')
-
-// parseFoodComposition()
+// "parse:FoodComposition": "node ./runner/parseFoodComposition",
+// "debug:FoodComposition": "npm run build && node ./debug/parseFoodComposition",
+//
+// // const { parseFoodComposition } = require('../dist/index.cjs')
+//
+// // parseFoodComposition()
--- projects2.0/USFA/Derivation_Code_Description/parser.js
+++ projects2.0/USFA/Derivation_Code_Description/parser.js
@@ -4,12 +4,12 @@
 // @TODO update require. when we export this method - we can connect it from index.js
 const {
   mainWrapper
-} = require('../../../../dist/index.cjs.js');
+} = require('../../../dist/index.cjs.js');
 
 async function Main() {
   const rawFilePath = `${__dirname}/Derivation_Code_Description.csv`;
   mainWrapper(rawFilePath)
-  mainWrapper(rawFilePath)
+  // mainWrapper(rawFilePath)
 
 }
 Main();
