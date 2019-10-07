e733de83bd4deca7ab2ab57c643b1be1cb675f1b
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-10-07 13:56:32 +0300

    test `csvToJson` in FAO/Fish

--- projects2.0/FAO/Fish/Fish_NV_Sum/parser.js
+++ projects2.0/FAO/Fish/Fish_NV_Sum/parser.js
@@ -1,11 +1,16 @@
 // const path = require('path');
 
 // @TODO update require. when we export this method - we can connect it from index.js
-const { mainWrapper } = require('../../../../dist/index.cjs');
+const {
+  mainWrapper,
+  parseCsv,
+  csvToJson
+} = require('../../../../dist/index.cjs');
 
 async function Main() {
   // const fileName = `Fish_NV_sum (per 100 g EP).csv`;
   const rawFilePath = `${__dirname}/Fish_NV_sum (per 100 g EP).csv`;
-  mainWrapper(rawFilePath);
+  const data = await parseCsv(rawFilePath);
+  await csvToJson(__dirname, data);
 }
 Main();
