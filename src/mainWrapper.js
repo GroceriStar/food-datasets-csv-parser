e1950bd8f2fdca96a9b35d803b296db345915892
sibasish14 <sibasish.ghosh14@gmail.com>
2019-10-14 09:33:17 +0530

    replaced contents of mainWrapper() with Main()

--- src/mainWrapper.js
+++ src/mainWrapper.js
@@ -1,8 +1,10 @@
 import path from 'path';
-import getHeaders from './getHeaders';
+// import getHeaders from './getHeaders';
+import csvToJson from './csvToJson';
+import parseCsv from './parseCsv';
 
 const mainWrapper = async (filename) => {
-    const headers = await getHeaders(filename);
+    // const headers = await getHeaders(filename);
   
     // console.log(headers);
     // I might have to include lines 5-14 insinde of an async function and await it.
@@ -12,20 +14,24 @@ const mainWrapper = async (filename) => {
     // projects we have a separated folder with files + parser inside.
     // we can make a root directory as default inside of `parserDirectoryFiles`
     // and change it if passed another variable
-    const directory = './';
+    // const directory = './';
   
-    const directoryPath = path.join(__dirname, directory + filename);
+    // const directoryPath = path.join(__dirname, directory + filename);
   
     // test version
     // const directoryPath = path.join(__dirname, filename);
   
     // console.log(filename);
   
-    console.log(directoryPath);
-    console.log('---');
-    console.log(headers);
+    // console.log(directoryPath);
+    // console.log('---');
+    // console.log(headers);
     // parseDirectoryFiles(directoryPath, headers);
     // parseDirectoryFiles(filename, headers);
+
+    const rawFilePath = path.join(__dirname, `Fish_Reftbl_RefDatasets.csv`);
+    const data = await parseCsv(rawFilePath);
+    await csvToJson(__dirname, data);
 };
 
 export {
