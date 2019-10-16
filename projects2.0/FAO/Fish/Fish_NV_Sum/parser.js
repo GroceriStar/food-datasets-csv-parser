b3f9683080a9e65937a114f1a604c8cf8a3e734d
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-10-16 23:49:58 +0300

    use `mainWrapper` fn in some parsers

--- projects2.0/FAO/Fish/Fish_NV_Sum/parser.js
+++ projects2.0/FAO/Fish/Fish_NV_Sum/parser.js
@@ -1,15 +1,4 @@
-const path = require('path');
-
 // @TODO update require. when we export this method - we can connect it from index.js
-const { csvToJson, parseCsv } = require('../../../../dist/index.cjs');
+const { mainWrapper } = require('../../../../dist/index.cjs');
 
-async function Main() {
-  // const fileName = `Fish_NV_sum (per 100 g EP).csv`;
-  const rawFilePath = `${__dirname}/Fish_NV_sum (per 100 g EP).csv`;
-  const data = await parseCsv(rawFilePath);
-  await csvToJson(__dirname, data);
-
-  // test json spliting
-  await csvToJson(__dirname, data, true);
-}
-Main();
+mainWrapper('Fish_NV_sum (per 100 g EP).csv', __dirname);
--- projects2.0/FAO/Fish/Fish_Reftbl_RefDatasets/parser.js
+++ projects2.0/FAO/Fish/Fish_Reftbl_RefDatasets/parser.js
@@ -1,13 +1,4 @@
-const path = require('path');
 // @TODO update require. when we export this method - we can connect it from index.js
-const { csvToJson, parseCsv } = require('../../../../dist/index.cjs');
+const { mainWrapper } = require('../../../../dist/index.cjs');
 
-async function Main() {
-  const rawFilePath = path.join(__dirname, `Fish_Reftbl_RefDatasets.csv`);
-  const data = await parseCsv(rawFilePath);
-  await csvToJson(__dirname, data);
-
-  // test json spliting
-  await csvToJson(__dirname, data, true);
-}
-Main();
+mainWrapper(`Fish_Reftbl_RefDatasets.csv`, __dirname);
