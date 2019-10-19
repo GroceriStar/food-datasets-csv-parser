c802dab7d7ca29eaa7c8458489997eba95f671af
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-10-19 14:25:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-// import { parseFoodComposition } from '../projects2.0/FoodComposition/parser';
 import csvToJson from './csvToJson';
-
 import parseCsv from './parseCsv';
 
-// @TODO i don't like this name,
-// because if we use it outside - it's very long
-// and it boring and we at this time have
-// some many copy-pasting it in projects.2.0
 
 // i adding utils here only because it's my guess
 import { readAllFiles, joinPath, getList } from './utils';
@@ -17,7 +11,6 @@ export {
   // @TODO let's debate should we export
   // parseCsv or we just using it inside of csvToJson
   parseCsv,
-  // parseFoodComposition,
   mainWrapper,
   // can be removed later
   readAllFiles,
