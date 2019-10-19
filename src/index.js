3c03311efbce2cf7722bf9066485ba76f873a388
sibasish14 <sibasish.ghosh14@gmail.com>
2019-10-19 11:16:40 +0530

    removed line containing parseDirectoryFiles

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import parseCsv from './parseCsv';
 // because if we use it outside - it's very long
 // and it boring and we at this time have
 // some many copy-pasting it in projects.2.0
-import parseDirectoryFiles from './fileSystem';
 
 // i adding utils here only because it's my guess
 import { readAllFiles, joinPath, getList } from './utils';
@@ -19,7 +18,6 @@ export {
   // parseCsv or we just using it inside of csvToJson
   parseCsv,
   // parseFoodComposition,
-  parseDirectoryFiles,
   mainWrapper,
   // can be removed later
   readAllFiles,
