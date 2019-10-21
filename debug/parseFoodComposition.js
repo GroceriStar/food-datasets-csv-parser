dcbb1922313baa1b027030787e7127573a688f30
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-10-21 06:38:16 +0300

    Delete parseFoodComposition.js

--- debug/parseFoodComposition.js
+++
@@ -1,6 +0,0 @@
-// "parse:FoodComposition": "node ./runner/parseFoodComposition",
-// "debug:FoodComposition": "npm run build && node ./debug/parseFoodComposition",
-//
-// // const { parseFoodComposition } = require('../dist/index.cjs')
-//
-// // parseFoodComposition()
