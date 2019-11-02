eb62ba4c59e4441d8615390f6b63813ac31309b5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-11-02 14:54:10 +0300

    release

--- package/src/index.js
+++ package/src/index.js
@@ -1,7 +1,6 @@
 import csvToJson from './csvToJson';
 import parseCsv from './parseCsv';
 
-
 // i adding utils here only because it's my guess
 import { readAllFiles, joinPath, getList } from './utils';
 import mainWrapper from './mainWrapper';
