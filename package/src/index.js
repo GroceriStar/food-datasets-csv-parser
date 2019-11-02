5ea5171f5d2037405f7f130479ce283fac36896e
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-11-02 13:01:55 +0300

    move `findCsvFiles` helper to a seperate file

--- package/src/index.js
+++ package/src/index.js
@@ -5,12 +5,14 @@ import parseCsv from './parseCsv';
 // i adding utils here only because it's my guess
 import { readAllFiles, joinPath, getList } from './utils';
 import mainWrapper from './mainWrapper';
+import findCsvFiles from './findCsvFiles';
 
 export {
   csvToJson,
   // @TODO let's debate should we export
   // parseCsv or we just using it inside of csvToJson
   parseCsv,
+  findCsvFiles,
   mainWrapper,
   // can be removed later
   readAllFiles,
